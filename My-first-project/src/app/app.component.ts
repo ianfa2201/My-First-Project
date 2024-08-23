ff80752612153056c0041995c18ff1320ccd7926
ianfa2201 <ianf.blancol@konradlorenz.edu.co>
2024-08-22 21:08:35 -0500

    Adiccion de nuevos componentes y actualizaci[on de estilos para el proyecto]

--- My-first-project/src/app/app.component.ts
+++ My-first-project/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TeacherComponent } from "./teacher/teacher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, TeacherComponent],
   templateUrl: './home/home.component.html',
-  styleUrl: './home/home.component.css',
+  styleUrl: './styles/bootstrap.min.css',
 })
 export class AppComponent {
   title = 'My-first-project';
